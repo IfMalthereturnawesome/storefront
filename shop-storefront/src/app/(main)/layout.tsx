558b774f55ea82db7f7833d7140e7462313bed00
IfMalthereturnawesome <malt996b@stud.kea.dk>
2023-09-17 22:56:45 +0200

    Backend: deployment fixing worked, but region not working, trying with default FOOTER

--- shop-storefront/src/app/(main)/layout.tsx
+++ shop-storefront/src/app/(main)/layout.tsx
@@ -5,6 +5,7 @@ import AOS from 'aos';
 import 'aos/dist/aos.css';
 import Footer from '@/components/ui/Footer';
 import Header from "@/components/ui/Header";
+import FooterNav from "@modules/layout/components/footer-nav";
 
 
 export default function PageLayout({
@@ -27,7 +28,7 @@ export default function PageLayout({
             <main className="grow bg-cyan-1">
                 {children}
             </main>
-            <Footer/>
+            <FooterNav/>
         </>
     )
 }
