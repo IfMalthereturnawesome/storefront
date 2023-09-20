37a96ebc26daa5c5fc155aaa1709ec7e46da7ea5
IfMalthereturnawesome <malt996b@stud.kea.dk>
2023-09-20 17:25:35 +0200

    Added homerunnerPingback.js

---
+++ shop-storefront/src/app/api/homerunner/homerunnerPingback.js
@@ -0,0 +1,18 @@
+// pages/api/homerunnerPingback.js
+
+export default async (req, res) => {
+  if (req.method === 'POST') {
+    // Process the incoming data from Homerunner
+    const data = req.body;
+
+    // TODO: Perform necessary actions like updating your database, etc.
+
+
+    // Send success response
+    res.status(200).json({ message: 'Successfully received data' });
+  } else {
+    // Handle any other HTTP methods
+    res.setHeader('Allow', ['POST']);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+};
