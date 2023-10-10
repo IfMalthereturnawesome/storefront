5f6ff3b25ae94fa7c08b03951e52d1fc4cdabf30
IfMalthereturnawesome <malt996b@stud.kea.dk>
2023-10-10 17:14:26 +0200

    Test for react email components for server transactional emails

--- shop-storefront/src/emails/renderEmail.js
+++
@@ -1,7 +0,0 @@
-import WelcomeEmail from './welcome.js';
-
-import { render } from "@react-email/render";
-
-export function getWelcomeEmailHtml(fullName) {
-  return render(<WelcomeEmail fullName={fullName} />);
-}
