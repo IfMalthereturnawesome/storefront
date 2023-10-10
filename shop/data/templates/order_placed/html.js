fb4afea72009a30c4727ecb6984c74edf4257044
IfMalthereturnawesome <malt996b@stud.kea.dk>
2023-10-10 19:25:47 +0200

    Email fix?

--- shop/data/templates/order_placed/html.js
+++ shop/data/templates/order_placed/html.js
@@ -1,18 +1,101 @@
 "use strict";
 
-var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+var _typeof = require("@babel/runtime/helpers/typeof");
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports["default"] = void 0;
-var _react = _interopRequireDefault(require("react"));
-var _orderPlaced = _interopRequireDefault(require("../../../react-email-starter/emails/order-placed.js"));
-var OrderPlacedEmail = function OrderPlacedEmail(_ref) {
+var React = _interopRequireWildcard(require("react"));
+var _components = require("@react-email/components");
+function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "function") return null; var cacheBabelInterop = new WeakMap(); var cacheNodeInterop = new WeakMap(); return (_getRequireWildcardCache = function _getRequireWildcardCache(nodeInterop) { return nodeInterop ? cacheNodeInterop : cacheBabelInterop; })(nodeInterop); }
+function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { "default": obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj["default"] = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
+var baseUrl = process.env.VERCEL_URL ? "https://".concat(process.env.VERCEL_URL) : '';
+var OrderPlacedTemplate = function OrderPlacedTemplate(_ref) {
   var customerName = _ref.customerName,
     orderNumber = _ref.orderNumber;
-  return /*#__PURE__*/_react["default"].createElement(_orderPlaced["default"], {
-    customerName: customerName,
-    orderNumber: orderNumber
-  });
+  return /*#__PURE__*/React.createElement(_components.Html, null, /*#__PURE__*/React.createElement(_components.Head, null, /*#__PURE__*/React.createElement("title", null, "Order Received")), /*#__PURE__*/React.createElement(_components.Preview, null, "We have received your order: ", orderNumber, "."), /*#__PURE__*/React.createElement(_components.Body, {
+    style: main
+  }, /*#__PURE__*/React.createElement(_components.Container, {
+    style: container
+  }, /*#__PURE__*/React.createElement(_components.Img, {
+    src: "".concat(baseUrl, "/static/eight-athletics-black-logo.png"),
+    width: "103",
+    height: "28",
+    alt: "Eight Atlhetics Logo",
+    style: logo
+  }), /*#__PURE__*/React.createElement(_components.Text, {
+    style: header
+  }, "Order Confirmation"), /*#__PURE__*/React.createElement(_components.Text, {
+    style: paragraph
+  }, "Hi ", customerName, ","), /*#__PURE__*/React.createElement(_components.Text, {
+    style: paragraph
+  }, "Thank you for your order! We've received your order #", orderNumber, " and will begin processing it shortly."), /*#__PURE__*/React.createElement(_components.Hr, {
+    style: hr
+  }), /*#__PURE__*/React.createElement(_components.Section, {
+    style: orderSummary
+  }, /*#__PURE__*/React.createElement(_components.Text, {
+    style: summaryHeader
+  }, "Order Summary:"), /*#__PURE__*/React.createElement(_components.Text, {
+    style: paragraph
+  }, "Order Number: ", orderNumber)), /*#__PURE__*/React.createElement(_components.Button, {
+    pX: 12,
+    pY: 12,
+    style: button,
+    href: "https://www.eightathletics.com"
+  }, "Visit our website"), /*#__PURE__*/React.createElement(_components.Hr, {
+    style: hr
+  }), /*#__PURE__*/React.createElement(_components.Text, {
+    style: footer
+  }, "Eight Athletics | Sofiegade 5, Copenhagen K, Denmark"))));
 };
-var _default = exports["default"] = OrderPlacedEmail;
+var _default = exports["default"] = OrderPlacedTemplate;
+var main = {
+  backgroundColor: '#ffffff',
+  fontFamily: '-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen-Sans,Ubuntu,Cantarell,"Helvetica Neue",sans-serif'
+};
+var container = {
+  margin: '0 auto',
+  padding: '20px 0 48px'
+};
+var logo = {
+  margin: '0 auto'
+};
+var header = {
+  fontSize: '20px',
+  fontWeight: 'bold',
+  textAlign: 'center'
+};
+var paragraph = {
+  fontSize: '16px',
+  lineHeight: '26px',
+  margin: '0 0 16px'
+};
+var hr = {
+  borderColor: '#cccccc',
+  margin: '20px 0'
+};
+var orderSummary = {
+  backgroundColor: '#f9f9f9',
+  padding: '16px',
+  borderRadius: '4px'
+};
+var summaryHeader = {
+  fontWeight: 'bold',
+  marginBottom: '8px'
+};
+var button = {
+  backgroundColor: '#5F51E8',
+  borderRadius: '3px',
+  color: '#fff',
+  fontSize: '16px',
+  textDecoration: 'none',
+  textAlign: 'center',
+  display: 'block',
+  margin: '0 auto 20px',
+  width: '200px'
+};
+var footer = {
+  color: '#8898aa',
+  fontSize: '12px',
+  textAlign: 'center'
+};
--- shop/data/templates/order_placed/html.jsx
+++ shop/data/templates/order_placed/html.jsx
@@ -1,11 +1,119 @@
-import React from 'react';
-import OrderPlacedTemplate
-  from '../../../react-email-starter/emails/order-placed.js';
+import * as React from 'react';
+import {
+  Body,
+  Button,
+  Container,
+  Head,
+  Hr,
+  Html,
+  Img,
+  Preview,
+  Section,
+  Text,
+} from '@react-email/components';
 
-const OrderPlacedEmail = ({ customerName, orderNumber }) => (
+const baseUrl = process.env.VERCEL_URL
+    ? `https://${process.env.VERCEL_URL}`
+    : '';
 
-    <OrderPlacedTemplate customerName={customerName} orderNumber={orderNumber} />
 
+const OrderPlacedTemplate = ({ customerName, orderNumber }) => (
+    <Html>
+      <Head>
+        <title>Order Received</title>
+      </Head>
+      <Preview>
+        We have received your order: {orderNumber}.
+      </Preview>
+      <Body style={main}>
+        <Container style={container}>
+
+          <Img
+              src={`${baseUrl}/static/eight-athletics-black-logo.png`}
+              width="103"
+              height="28"
+              alt="Eight Atlhetics Logo"
+              style={logo}
+          />
+          <Text style={header}>Order Confirmation</Text>
+          <Text style={paragraph}>Hi {customerName},</Text>
+          <Text style={paragraph}>
+            Thank you for your order! We've received your order #{orderNumber} and will begin processing it shortly.
+          </Text>
+          <Hr style={hr} />
+          <Section style={orderSummary}>
+            <Text style={summaryHeader}>Order Summary:</Text>
+            {/* You can add more details about the order here */}
+            <Text style={paragraph}>Order Number: {orderNumber}</Text>
+          </Section>
+          <Button pX={12} pY={12} style={button} href="https://www.eightathletics.com">
+            Visit our website
+          </Button>
+          <Hr style={hr} />
+          <Text style={footer}>Eight Athletics | Sofiegade 5, Copenhagen K, Denmark</Text>
+        </Container>
+      </Body>
+    </Html>
 );
 
-export default OrderPlacedEmail;
+export default OrderPlacedTemplate;
+
+const main = {
+  backgroundColor: '#ffffff',
+  fontFamily: '-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen-Sans,Ubuntu,Cantarell,"Helvetica Neue",sans-serif',
+};
+
+const container = {
+  margin: '0 auto',
+  padding: '20px 0 48px',
+};
+
+const logo = {
+  margin: '0 auto',
+};
+
+const header = {
+  fontSize: '20px',
+  fontWeight: 'bold',
+  textAlign: 'center',
+};
+
+const paragraph = {
+  fontSize: '16px',
+  lineHeight: '26px',
+  margin: '0 0 16px',
+};
+
+const hr = {
+  borderColor: '#cccccc',
+  margin: '20px 0',
+};
+
+const orderSummary = {
+  backgroundColor: '#f9f9f9',
+  padding: '16px',
+  borderRadius: '4px',
+};
+
+const summaryHeader = {
+  fontWeight: 'bold',
+  marginBottom: '8px',
+};
+
+const button = {
+  backgroundColor: '#5F51E8',
+  borderRadius: '3px',
+  color: '#fff',
+  fontSize: '16px',
+  textDecoration: 'none',
+  textAlign: 'center',
+  display: 'block',
+  margin: '0 auto 20px',
+  width: '200px',
+};
+
+const footer = {
+  color: '#8898aa',
+  fontSize: '12px',
+  textAlign: 'center',
+};
