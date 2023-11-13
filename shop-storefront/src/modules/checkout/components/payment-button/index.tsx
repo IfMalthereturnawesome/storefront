ec3fbacda96a432f541cba8d372ca4270e3369f0
IfMalthereturnawesome <malt996b@stud.kea.dk>
2023-11-13 17:24:08 +0100

    Adding better Error message for payment - more

--- shop-storefront/src/modules/checkout/components/payment-button/index.tsx
+++ shop-storefront/src/modules/checkout/components/payment-button/index.tsx
@@ -1,170 +1,169 @@
-import { useCheckout } from "@lib/context/checkout-context"
-import { PaymentSession } from "@medusajs/medusa"
+import {useCheckout} from "@lib/context/checkout-context"
+import {PaymentSession} from "@medusajs/medusa"
 import Button from "@modules/common/components/button"
 import Spinner from "@modules/common/icons/spinner"
-import { OnApproveActions, OnApproveData } from "@paypal/paypal-js"
-import { PayPalButtons, PayPalScriptProvider } from "@paypal/react-paypal-js"
-import { useElements, useStripe } from "@stripe/react-stripe-js"
-import { useCart } from "medusa-react"
-import React, { useEffect, useState } from "react"
+import {OnApproveActions, OnApproveData} from "@paypal/paypal-js"
+import {PayPalButtons, PayPalScriptProvider} from "@paypal/react-paypal-js"
+import {useElements, useStripe} from "@stripe/react-stripe-js"
+import {useCart} from "medusa-react"
+import React, {useEffect, useState} from "react"
 import BuyNowButton from "@/components/elements/BuyNowButton";
 import cart from "@modules/common/icons/cart";
-import { ExclamationCircleIcon } from '@heroicons/react/24/solid';
+import {ExclamationCircleIcon} from '@heroicons/react/24/solid';
 import ErrorModal from "@modules/error/ErrorModal";
 
 type PaymentButtonProps = {
-  paymentSession?: PaymentSession | null
+    paymentSession?: PaymentSession | null
 }
 
-const PaymentButton: React.FC<PaymentButtonProps> = ({ paymentSession }) => {
-  const [notReady, setNotReady] = useState(true)
-  const { cart } = useCart()
-  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+const PaymentButton: React.FC<PaymentButtonProps> = ({paymentSession}) => {
+    const [notReady, setNotReady] = useState(true)
+    const {cart} = useCart()
+    const [errorMessage, setErrorMessage] = useState<string | null>(null);
 
 
-  useEffect(() => {
-    setNotReady(true);
+    useEffect(() => {
+        setNotReady(true);
 
-    console.log(cart.shipping_address)
-    if (!cart) {
-      setErrorMessage("Cart is not available.");
-      return;
-    }
+        console.log(cart.shipping_address)
+        if (!cart) {
+            setErrorMessage("Cart is not available.");
+            return;
+        }
 
-    if (!cart.shipping_address) {
-      setErrorMessage("Shipping address is required.");
-      return;
-    }
+        if (!cart.shipping_address) {
+            setErrorMessage("Shipping address is required.");
+            return;
+        }
 
-    if (!cart.email) {
-      setErrorMessage("Email is required.");
-      return;
-    }
+        if (!cart.email) {
+            setErrorMessage("Email is required.");
+            return;
+        }
 
-    if (cart.shipping_methods.length < 1) {
-      setErrorMessage("Shipping method is required.");
-      return;
-    }
+        if (cart.shipping_methods.length < 1) {
+            setErrorMessage("Shipping method is required.");
+            return;
+        }
 
-    setNotReady(false);
-    setErrorMessage(null);
-  }, [cart]);
+        setNotReady(false);
+        setErrorMessage(null);
+    }, [cart]);
 
 
-  return (
-      <div>
-        {errorMessage && (
-            <div className="text-red-500 text-small-regular mt-2">
-              {errorMessage}
-            </div>
-        )}
+    return (
+        <div>
+            {errorMessage && (
+                <div className="flex items-center text-red-500 text-small-regular mt-2 animate-fade-in">
+                    <ExclamationCircleIcon className="h-5 w-5 mr-2"/>
+                    {errorMessage}
+                </div>
+            )}
 
-        {/* existing switch case logic for rendering the payment buttons */}
-        {(() => {
-          switch (paymentSession?.provider_id) {
-            case "stripe":
-              return (
-                  <StripePaymentButton session={paymentSession} notReady={notReady} />
-              );
+            {/* existing switch case logic for rendering the payment buttons */}
+            {(() => {
+                switch (paymentSession?.provider_id) {
+                    case "stripe":
+                        return (
+                            <StripePaymentButton session={paymentSession} notReady={notReady}/>
+                        );
 
-          }
-        })()}
-      </div>
-  );
+                }
+            })()}
+        </div>
+    );
 }
 
 type StripePaymentButtonProps = {
-  session: PaymentSession;
-  notReady: boolean;
+    session: PaymentSession;
+    notReady: boolean;
 
 };
 
 const StripePaymentButton = ({
-                               session,
-                               notReady,
+                                 session,
+                                 notReady,
 
                              }: StripePaymentButtonProps) => {
 
-  const [disabled, setDisabled] = useState(false)
-  const [submitting, setSubmitting] = useState(false)
-  const [errorMessage, setErrorMessage] = useState<string | undefined>(
-      undefined
-  )
-  const [showModal, setShowModal] = useState(false);
-  const { cart } = useCart()
-  const { onPaymentCompleted } = useCheckout()
+    const [disabled, setDisabled] = useState(false)
+    const [submitting, setSubmitting] = useState(false)
+    const [errorMessage, setErrorMessage] = useState<string | undefined>(
+        undefined
+    )
+    const [showModal, setShowModal] = useState(false);
+    const {cart} = useCart()
+    const {onPaymentCompleted} = useCheckout()
 
-  const stripe = useStripe()
-  const elements = useElements()
-
-
-  useEffect(() => {
-    if (!stripe || !elements) {
-      setDisabled(true)
-    } else {
-      setDisabled(false)
-    }
-  }, [stripe, elements])
-
-  const handlePayment = async (e) => {
-      e.preventDefault()
-    setSubmitting(true)
-
-      if (!stripe || !elements  || !cart ) {
-          setSubmitting(false)
-          return
-      }
-
-      const return_url = `${window.location.origin}/order-processing?cart_id=${cart.id}`;
+    const stripe = useStripe()
+    const elements = useElements()
 
 
-      const result = await stripe.confirmPayment({
-          elements,
+    useEffect(() => {
+        if (!stripe || !elements) {
+            setDisabled(true)
+        } else {
+            setDisabled(false)
+        }
+    }, [stripe, elements])
+
+    const handlePayment = async (e) => {
+        e.preventDefault()
+        setSubmitting(true)
+
+        if (!stripe || !elements || !cart) {
+            setSubmitting(false)
+            return
+        }
+
+        const return_url = `${window.location.origin}/order-processing?cart_id=${cart.id}`;
+
+
+        const result = await stripe.confirmPayment({
+            elements,
             confirmParams: {
                 return_url: return_url,
             },
 
 
-      });
+        });
 
-      if (result.error) {
-        setShowModal(true);
-          setErrorMessage(result.error.message);
-      } else {
+        if (result.error) {
+            setShowModal(true);
+            setErrorMessage(result.error.message);
+        } else {
 
-      }
+        }
 
-      setSubmitting(false);
-  }
-
+        setSubmitting(false);
+    }
 
 
     return (
 
-      <>
-        <BuyNowButton
-            message={"Place Order"}
-            disabled={submitting || disabled || notReady}
-            // @ts-ignore
-            onClick={handlePayment}
-            title="Place Order"
-            className={"truncate"}
-        >
-          {submitting ? <Spinner /> : "Checkout"}
-        </BuyNowButton>
-        {errorMessage && showModal && (
-            <div className="flex items-center text-red-500 text-small-regular mt-2 animate-fade-in">
-              <ExclamationCircleIcon className="h-5 w-5 mr-2" />
-              {errorMessage}
-              <ErrorModal message={errorMessage || ''}   isOpen={showModal} onClose={() => setShowModal(false)}/>
-            </div>
+        <>
+            <BuyNowButton
+                message={"Place Order"}
+                disabled={submitting || disabled || notReady}
+                // @ts-ignore
+                onClick={handlePayment}
+                title="Place Order"
+                className={"truncate"}
+            >
+                {submitting ? <Spinner/> : "Checkout"}
+            </BuyNowButton>
+            {errorMessage && showModal && (
+                <div className="flex items-center text-red-500 text-small-regular mt-2 animate-fade-in">
+                    <ExclamationCircleIcon className="h-5 w-5 mr-2"/>
+                    {errorMessage}
+                    <ErrorModal message={errorMessage || ''} isOpen={showModal} onClose={() => setShowModal(false)}/>
+                </div>
 
-        )}
+            )}
 
 
-
-      </>
-  )
+        </>
+    )
 }
 //
 // const PAYPAL_CLIENT_ID = process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID || ""
@@ -225,24 +224,24 @@ const StripePaymentButton = ({
 //   )
 // }
 
-const ManualTestPaymentButton = ({ notReady }: { notReady: boolean }) => {
-  const [submitting, setSubmitting] = useState(false)
+const ManualTestPaymentButton = ({notReady}: { notReady: boolean }) => {
+    const [submitting, setSubmitting] = useState(false)
 
-  const { onPaymentCompleted } = useCheckout()
+    const {onPaymentCompleted} = useCheckout()
 
-  const handlePayment = () => {
-    setSubmitting(true)
+    const handlePayment = () => {
+        setSubmitting(true)
 
-    onPaymentCompleted()
+        onPaymentCompleted()
 
-    setSubmitting(false)
-  }
+        setSubmitting(false)
+    }
 
-  return (
-      <Button disabled={submitting || notReady} onClick={handlePayment}>
-        {submitting ? <Spinner /> : "Checkout"}
-      </Button>
-  )
+    return (
+        <Button disabled={submitting || notReady} onClick={handlePayment}>
+            {submitting ? <Spinner/> : "Checkout"}
+        </Button>
+    )
 }
 
 export default PaymentButton
