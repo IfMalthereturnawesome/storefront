b61a3a00f31dc2693992d2621fe0054315707070
IfMalthereturnawesome <malt996b@stud.kea.dk>
2023-12-03 20:09:37 +0100

    Small fixes to next.config.js and added stuff to gitignore

--- shop-storefront/next.config.js
+++ shop-storefront/next.config.js
@@ -4,6 +4,33 @@ const store = require('./store.config.json');
 /**
  * @type {import('next').NextConfig}
  */
+
+const advancedHeaders = [
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on',
+  },
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=63072000; includeSubDomains; preload',
+  },
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block',
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN',
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'origin-when-cross-origin',
+  },
+];
 const nextConfig = {
   swcMinify: true,
   experimental: {
@@ -14,25 +41,30 @@ const nextConfig = {
     ],
 
   },
+  transpilePackages: ['@mui/system', '@mui/material', '@mui/icons-material'],
   modularizeImports: {
-    '@mui/material': {
-      transform: '@mui/material/{{member}}',
+    '@mui/material/?(((\\w*)?/?)*)': {
+      transform: '@mui/material/{{ matches.[1] }}/{{member}}',
     },
-    '@mui/icons-material': {
-      transform: '@mui/icons-material/{{member}}',
+    '@mui/icons-material/?(((\\w*)?/?)*)': {
+      transform: '@mui/icons-material/{{ matches.[1] }}/{{member}}',
     },
   },
   features: store.features,
   reactStrictMode: true,
   images: {
     domains: [
-      'medusa-public-images.s3.eu-west-1.amazonaws.com',
       'localhost',
-      'medusa-server-testing.s3.amazonaws.com',
     ],
+  },
+  headers: async () => {
+    return [
+      {
+        source: '/:path*',
+        headers: advancedHeaders,
+      },
+    ];
   },
 };
 
 module.exports = withStoreConfig(withContentlayer(nextConfig));
-
-console.log('next.config.js', JSON.stringify(module.exports, null, 2));
