a6df660b7b3c6551aed269e26cdae954144af058
IfMalthereturnawesome <malt996b@stud.kea.dk>
2023-12-05 01:39:34 +0100

    Fix deported into client side add Fallback to suspense with animate spin

--- shop-storefront/src/modules/providers/index.tsx
+++ shop-storefront/src/modules/providers/index.tsx
@@ -6,13 +6,24 @@ import { CartDropdownProvider } from "@lib/context/cart-dropdown-context"
 import { MobileMenuProvider } from "@lib/context/mobile-menu-context"
 import { StoreProvider } from "@lib/context/store-context"
 import { MedusaProvider, CartProvider } from "medusa-react"
-import {Suspense} from "react";
+import React, {Suspense} from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 
 export default function Providers({ children }: { children: React.ReactNode }) {
 
   function Fallback() {
-    return <>placeholder</>
+    return (
+        <div className="flex items-center justify-center h-screen ">
+
+
+          <div className="flex  items-center  justify-center lg:justify-start">
+            <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-8">
+            </div>
+          </div>
+        </div>
+    )
   }
   return (
 
