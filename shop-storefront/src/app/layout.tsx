ab7482edc673a07277c1664f974d42cb897808a9
IfMalthereturnawesome <malt996b@stud.kea.dk>
2023-12-19 20:18:40 +0100

    Add GTM

--- shop-storefront/src/app/layout.tsx
+++ shop-storefront/src/app/layout.tsx
@@ -78,7 +78,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','${GTAG}');
+        })(window,document,'script','dataLayer','GTM-TRH2KS6X');
         `}
       </Script>
       <Suspense fallback={<Fallback />}>
@@ -107,7 +107,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
           <SpeedInsights />
           <noscript
             dangerouslySetInnerHTML={{
-              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${GTAG}" height="0" width="0" style="display: none; visibility: hidden;"></iframe>`,
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TRH2KS6X" height="0" width="0" style="display: none; visibility: hidden;"></iframe>`,
             }}
           />
         </body>
