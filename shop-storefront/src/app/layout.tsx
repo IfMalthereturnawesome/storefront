e9aae2861af60bcf03b9dfc26b674eba86afc51a
IfMalthereturnawesome <malt996b@stud.kea.dk>
2023-12-19 19:58:32 +0100

    Add Google tag manager

--- shop-storefront/src/app/layout.tsx
+++ shop-storefront/src/app/layout.tsx
@@ -10,6 +10,7 @@ import BreakpointsIndicatorExtended from "@/components/helper/breakpointsExtende
 import BreakpointsIndicator from "@/components/helper/breakpoints";
 import React, {Suspense} from "react";
 import {PHProvider, PostHogPageview} from "./provider";
+import Script from 'next/script'
 
 const GTAG = process.env.NEXT_PUBLIC_MEASUREMENT_ID;
 
@@ -70,6 +71,15 @@ export default function RootLayout({children}: RootLayoutProps) {
 
     return (
         <html lang="en" className="dark" style={{colorScheme: 'dark'}}>
+        <Script id="google-tag-manager" strategy="afterInteractive">
+            {`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','${GTAG}');
+        `}
+        </Script>
         <Suspense fallback={<Fallback/>}>
             <PostHogPageview/>
         </Suspense>
@@ -96,7 +106,11 @@ export default function RootLayout({children}: RootLayoutProps) {
                 <GoogleAnalytics GA_MEASUREMENT_ID={GTAG}/>
                 <CookieBanner/>
             </Suspense>
-
+            <noscript
+                dangerouslySetInnerHTML={{
+                    __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${GTAG}" height="0" width="0" style="display: none; visibility: hidden;"></iframe>`,
+                }}
+            />
             </body>
         </PHProvider>
         </html>
