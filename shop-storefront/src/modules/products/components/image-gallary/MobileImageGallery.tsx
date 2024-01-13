d1971acf80dccb096999ca680aeeb810f297c7c9
IfMalthereturnawesome <malt996b@stud.kea.dk>
2024-01-13 15:30:04 +0100

    Remove Swiper library

--- shop-storefront/src/modules/products/components/image-gallary/MobileImageGallery.tsx
+++ shop-storefront/src/modules/products/components/image-gallary/MobileImageGallery.tsx
@@ -1,58 +1,102 @@
-import React, { useState } from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Thumbs, Zoom, Pagination, FreeMode } from 'swiper/modules';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/thumbs';
-import 'swiper/css/zoom';
-import 'swiper/css/pagination';
-import Image from "next/image";
+import React, { useState, useRef } from 'react';
+import Image from 'next/image';
 
-const MobileImageGallery = ({ images }) => {
-    const [thumbsSwiper, setThumbsSwiper] = useState(null);
+const MobileImageCarousel = ({ images }) => {
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const carouselRef = useRef(null);
+    const isPrevDisabled = currentIndex === 0;
+    const isNextDisabled = currentIndex === images.length - 1;
+
+
+    const handleSwipe = (direction) => {
+        if (direction === 'left' && !isNextDisabled) {
+            setCurrentIndex(currentIndex + 1);
+        } else if (direction === 'right' && !isPrevDisabled) {
+            setCurrentIndex(currentIndex - 1);
+        }
+    };
+
+    const handleTouchStart = (e) => {
+        const touchDown = e.touches[0].clientX;
+        carouselRef.current = touchDown;
+    };
+
+    const handleTouchMove = (e) => {
+        const touchDown = carouselRef.current;
+        if (touchDown === null) return;
+
+        const currentTouch = e.touches[0].clientX;
+        const diff = touchDown - currentTouch;
+
+        if (diff > 5) {
+            handleSwipe('left');
+        } else if (diff < -5) {
+            handleSwipe('right');
+        }
+
+        carouselRef.current = null;
+    };
 
     return (
-        <>
-            <Swiper
-                style={{
-                    '--swiper-navigation-color': '#524f4f',
-                    '--swiper-pagination-color': '#524c4c',
-                    '--swiper-navigation-size': '24px',
-                } as React.CSSProperties}
-                modules={[Zoom, Navigation, Thumbs, Pagination, FreeMode]}
-                spaceBetween={5}
-                navigation={true}
-                zoom
-                thumbs={{ swiper: thumbsSwiper }}
-                pagination={{ clickable: true }}
-                className="mySwiper2 w-auto m-0"
+        <div className="relative flex flex-col items-center justify-center w-full bg-[#E8EDEF]">
+            <div
+                onTouchStart={handleTouchStart}
+                onTouchMove={handleTouchMove}
+                className="overflow-hidden w-full h-[80%]"
             >
-                {images.map((image, index) => (
-                    <SwiperSlide key={image}>
-                        <div className="swiper-zoom-container w-[50%]">
-                            <Image height={460} width={400} src={image} alt={`Slide ${index}`} className={"fade-in "} quality={92}  />
-                        </div>
-                    </SwiperSlide>
-                ))}
-            </Swiper>
+                <div
+                    className="flex    "
+                    style={{ transform: `translateX(-${currentIndex * 100}%)` }}
+                >
+                    {images.map((image, index) => (
+                        <Image
+                            key={index}
+                            src={image}
+                            width={500}
+                            height={500}
+                            priority={index === 0}
+                            alt={`Slide ${index}`}
+                            className={`block w-full h-full object-cover ${currentIndex === index ?
+                                'opacity-100' : 'opacity-0'}`}
+                        />
+                    ))}
 
-            <Swiper
-                onSwiper={setThumbsSwiper}
-                spaceBetween={5}
-                slidesPerView={4}
-                freeMode
-                watchSlidesProgress
-                modules={[Thumbs]}
-                className="mySwiper"
+                </div>
+            </div>
+
+            <button
+                onClick={() => handleSwipe('right')}
+                className={`absolute left-0 z-30 p-2 text-black/50 bg-opacity-50 hover:bg-opacity-75 transition-opacity duration-300 ${isPrevDisabled ? 'opacity-50 cursor-default' : ''}`}
+                aria-label="Previous"
+                disabled={isPrevDisabled}
             >
-                {images.map((image, index) => (
-                    <SwiperSlide key={image}>
-                        <Image height={200} width={100} quality={50} src={image} alt={`Thumbnail ${index}`} className={"fade-in"}/>
-                    </SwiperSlide>
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 19l-7-7 7-7" />
+                </svg>
+            </button>
+
+            <button
+                onClick={() => handleSwipe('left')}
+                className={`absolute right-0 z-30 p-2 text-black/50 bg-opacity-50 hover:bg-opacity-75 transition-opacity duration-300 ${isNextDisabled ? 'opacity-50 cursor-default' : ''}`}
+                aria-label="Next"
+                disabled={isNextDisabled}
+            >
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5l7 7-7 7" />
+                </svg>
+            </button>
+            <div className="absolute bottom-0 left-0 right-0 z-30 flex justify-center p-2">
+                {images.map((_, index) => (
+                    <button
+                        key={index}
+                        onClick={() => setCurrentIndex(index)}
+                        className={`mx-1 h-2 w-2 rounded-full ${currentIndex === index ? 'bg-gray-700' : 'bg-gray-400'} transition duration-300`}
+                        aria-label={`Go to slide ${index + 1}`}
+                    />
                 ))}
-            </Swiper>
-        </>
+            </div>
+        </div>
     );
 };
 
-export default MobileImageGallery;
+export default MobileImageCarousel;
